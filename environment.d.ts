166aa3a641479f6d542c021077f6e921bb8115b6
Raphael Thurnherr <raphael.thurnherr1990@gmail.com>
2022-10-18 17:55:04 +0200

    init

---
+++ environment.d.ts
@@ -0,0 +1,13 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      RPC_URL: string
+      MNEMONIC_TEST_ALICE: string
+      ADDRESS_TEST_ALICE: string
+      MNEMONIC_TEST_BOB: string
+      ADDRESS_TEST_BOB: string
+    }
+  }
+}
+
+export {}
